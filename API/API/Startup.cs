50f9b341ebc59feaef875ea69a47ff90bb6e6efe
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2021-02-19 21:51:09 +0300

    Swagger added

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -42,6 +42,8 @@ namespace API
                     .AllowAnyHeader());
             });
 
+            services.AddSwaggerGen();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,7 +67,14 @@ namespace API
                 endpoints.MapControllers();
             });
 
-            
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+
         }
     }
 }
