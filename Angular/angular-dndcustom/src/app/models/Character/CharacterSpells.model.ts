b54fd8266626af8240814b9845ef139475384a79
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2021-02-21 10:28:30 +0300

    Character Spells service added angular

---
+++ Angular/angular-dndcustom/src/app/models/Character/CharacterSpells.model.ts
@@ -0,0 +1,7 @@
+export interface CharacterSpells {
+    Ref : number,
+    CharacterRef : number,
+    SpellRef : number,
+    Hit : number,
+    Damage : number
+};
---
+++ Angular/angular-dndcustom/src/app/services/CharacterServices/CharacterSpells.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core"
+import { CharacterSpells } from "src/app/models/Character/CharacterSpells.model"
+import { HttpService } from "../http.service"
+
+
+
+@Injectable({
+    providedIn: "root"
+})
+export class CharacterSpellsService {
+constructor(readonly httpService: HttpService){
+}
+
+public getCharactersSpells(): Promise<CharacterSpells[]> {
+  return  new Promise<CharacterSpells[]>((resolve, reject) => {
+    this.httpService.get('CharacterSpells', 'GetCharactersSpells').subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+
+public getCharacterSpells(characterRef : number): Promise<CharacterSpells> {
+  return  new Promise<CharacterSpells>((resolve, reject) => {
+    this.httpService.get('CharacterSpells', 'GetCharacterSpells', {characterRef}).subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+
+public deleteCharacterSpells(ref : number): Promise<any> {
+  return  new Promise<any>((resolve, reject) => {
+    this.httpService.delete('CharacterSpells', 'DeleteCharacterSpells', {ref}).subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+
+public insertOrUpdateCharacterSpells(model : CharacterSpells): Promise<number> {
+  return  new Promise<number>((resolve, reject) => {
+    this.httpService.post('CharacterSpells', 'InsertOrUpdateCharacterSpells',model).subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+}
