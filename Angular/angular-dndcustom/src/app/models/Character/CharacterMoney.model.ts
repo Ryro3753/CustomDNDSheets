b73c5f10cef585c31bfd0425c0ff0fd453444f7f
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2021-02-21 10:03:19 +0300

    Angular CharacterMoney service added

---
+++ Angular/angular-dndcustom/src/app/models/Character/CharacterMoney.model.ts
@@ -0,0 +1,7 @@
+export interface CharacterMoney {
+    Ref : number,
+    CharacterRef : number,
+    Copper : number,
+    Silver : number,
+    Gold : number
+};
---
+++ Angular/angular-dndcustom/src/app/services/CharacterServices/CharacterMoney.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core"
+import { CharacterMoney } from "src/app/models/Character/CharacterMoney.model"
+import { HttpService } from "../http.service"
+
+
+
+@Injectable({
+    providedIn: "root"
+})
+export class CharacterMoneyService {
+constructor(readonly httpService: HttpService){
+}
+
+public getCharactersMoney(): Promise<CharacterMoney[]> {
+  return  new Promise<CharacterMoney[]>((resolve, reject) => {
+    this.httpService.get('CharacterMoney', 'GetCharactersMoney').subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+
+public getCharacterMoney(characterRef : number): Promise<CharacterMoney> {
+  return  new Promise<CharacterMoney>((resolve, reject) => {
+    this.httpService.get('CharacterMoney', 'GetCharacterMoney', {characterRef}).subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+
+public deleteCharacterMoney(ref : number): Promise<any> {
+  return  new Promise<any>((resolve, reject) => {
+    this.httpService.delete('CharacterMoney', 'DeleteCharacterMoney', {ref}).subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+
+public insertOrUpdateCharacterMoney(model : CharacterMoney): Promise<number> {
+  return  new Promise<number>((resolve, reject) => {
+    this.httpService.post('CharacterMoney', 'InsertOrUpdateCharacterMoney',model).subscribe(data => {
+      resolve(data)
+    }, err => { reject(err) })
+  })
+}
+}
