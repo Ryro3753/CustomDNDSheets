d51fdea09b3dd9e9e1a787c4f8bb11af3c8e2f1e
İbrahim Özdemir <ibrahim.ozdemir@outlook.com.tr>
2021-02-21 18:43:47 +0300

    nebular theme added

--- Angular/angular-dndcustom/src/app/app.module.ts
+++ Angular/angular-dndcustom/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbActionsModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbActionsModule,
   ],
   providers: [
   ],
